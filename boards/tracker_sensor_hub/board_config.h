20f7224d4f6e3f455159c528e0072e8947059aa7
Dan Kouba <dan.kouba@particle.io>
2020-12-10 23:04:45 -0800

    Add Tracker One Sensor Hub bootloader config

---
+++ boards/tracker_sensor_hub/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Spark Products"
+#define PRODUCT_NAME "Tracker Sensor Hub"
+#define VOLUME_LABEL "T1HUBBOOT"
+#define INDEX_URL "http://particle.io/"
+#define BOARD_ID "Tracker_Sensor_Hub"
+
+#define USB_VID 0x2B04      // Spark products VID
+#define USB_PID 0x6020      // VID for platform ID 0x20 (not yet assigned to any Particle device)
+
+#define LED_PIN PIN_PA06    // Green LED
+
+#endif
